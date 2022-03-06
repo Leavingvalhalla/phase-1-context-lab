2148173cd0d0f3509d22c0ab4740804c39b15421
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-06 08:48:15 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord(array) {
+  this.firstName = array[0];
+  this.familyName = array[1];
+  this.title = array[2];
+  this.payPerHour = array[3];
+  this.timeInEvents = [];
+  this.timeOutEvents = [];
 }
 
+function createEmployeeRecords(array) {
+  const ans = array.map((employee) =>
+    createEmployeeRecord.call(this, employee)
+  );
+}
+
+const allWagesFor = function () {
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
+
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+  return payable;
+};
+
+const testEmployee = ['Zaphod', 'Beeblebrox', 'President', '1000'];
