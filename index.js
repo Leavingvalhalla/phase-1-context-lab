b8e5eecd66bf3d9569fdce9a6f57ee83ee649b4b
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-06 09:33:11 -0800

    finish lab

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function createEmployeeRecord(array) {
-  this.firstName = array[0];
-  this.familyName = array[1];
-  this.title = array[2];
-  this.payPerHour = array[3];
-  this.timeInEvents = [];
-  this.timeOutEvents = [];
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
 }
 
 function createEmployeeRecords(array) {
-  const ans = array.map((employee) =>
-    createEmployeeRecord.call(this, employee)
-  );
+  const newArray = [];
+  array.map((employee) => newArray.push(createEmployeeRecord(employee)));
+  return newArray;
 }
 
 const allWagesFor = function () {
@@ -28,4 +30,55 @@ const allWagesFor = function () {
   return payable;
 };
 
-const testEmployee = ['Zaphod', 'Beeblebrox', 'President', '1000'];
+function createTimeInEvent(event) {
+  const splitEvent = event.split(' ');
+  const newEvent = {
+    type: 'TimeIn',
+    date: splitEvent[0],
+    hour: parseInt(splitEvent[1]),
+  };
+  this.timeInEvents.push(newEvent);
+  return this;
+}
+
+function createTimeOutEvent(event) {
+  const splitEvent = event.split(' ');
+  const newEvent = {
+    type: 'TimeOut',
+    date: splitEvent[0],
+    hour: parseInt(splitEvent[1]),
+  };
+  this.timeOutEvents.push(newEvent);
+  return this;
+}
+
+const testEmployee = ['Zaphod', 'Beeblebrox', 'President', 1000];
+const employeeRecord = createEmployeeRecord(testEmployee);
+
+createTimeInEvent.call(employeeRecord, '2044-03-14 0900');
+createTimeInEvent.call(employeeRecord, '2044-03-15 0900');
+createTimeOutEvent.call(employeeRecord, '2044-03-14 2100');
+createTimeOutEvent.call(employeeRecord, '2044-03-15 2100');
+
+console.log(allWagesFor.call(employeeRecord));
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((e) => e.date.split(' ')[0] == date);
+  const timeOut = this.timeOutEvents.find((e) => e.date.split(' ')[0] == date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find((employee) => employee.firstName === firstName);
+}
+
+function calculatePayroll(array) {
+  return array.reduce(
+    (total, employee) => total + allWagesFor.call(employee),
+    0
+  );
+}
